7798fc326fa4dd9356821a6bcfa6f3a8d0c3aa54
Andrius <alimasWebDeveloper@gmail.com>
2021-06-05 14:06:44 +0100

    Update index.js and style.css
    Update index.js and style.css: in index.js i get navLinks objects as array and i go each single link object to create an event listener of click and when we click we will remove this nav-open class.
    
    In style.css i added smooth scroll bahavor.

--- js/index.js
+++ js/index.js
@@ -1,6 +1,15 @@
 // get toggle nav button object
 const navToggle = document.querySelector(".nav-toggle");
 
+// get navigation links objects
+const navLinks = document.querySelectorAll(".nav__link");
+
 navToggle.addEventListener("click", () => {
   document.body.classList.toggle("nav-open");
+});
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    document.body.classList.remove("nav-open");
+  });
 });
